f7437d0bc8d674a753b775c745741f92bbaddbb2
peterwilliams97 <peter.williams.97#gmail.com>
2013-12-30 14:13:13 +1100

    first checkin

---
+++ find_k.py
@@ -0,0 +1,268 @@
+"""
+    http://datasciencelab.wordpress.com/2013/12/12/clustering-with-k-means-in-python/
+    http://datasciencelab.wordpress.com/2013/12/27/finding-the-k-in-k-means-clustering/
+"""
+from __future__ import division
+import sys, random
+import numpy as np
+from collections import defaultdict
+
+
+def cluster_points(X, mu):
+    clusters = defaultdict(list)
+    #print mu   
+    for x in X:
+        #print np.apply_along_axis(np.linalg.norm, 1, x - mu)
+        #print np.linalg.norm(x - mu, ord=None, axis=0)
+        i = np.argmin(np.apply_along_axis(np.linalg.norm, 1, x - mu)) 
+        #print k
+        clusters[i].append(x)
+     
+    #for k, v in clusters.items():
+    #    print k, len(v)
+    return clusters
+
+ 
+def reevaluate_centers(clusters):
+    """
+        mu: a np array
+        clusters: dict of np arrays
+    """
+    #print 'mu'
+    #print mu
+    #print 'clusters'
+    #print clusters
+    #exit()
+    keys = sorted(clusters.keys())
+    return np.array([np.mean(clusters[k], axis=0) for k in keys])
+
+ 
+def has_converged(mu, oldmu):
+    return set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
+
+ 
+def find_centers(X, k):
+  
+    # Initialize to K random centers
+    mu = random.sample(X, k)
+
+    assert len(mu) == k, mu
+
+    while True:
+        assert len(mu) == k, mu
+        oldmu = mu
+        # Assign all points in X to clusters
+        clusters = cluster_points(X, mu)
+        assert len(clusters) == k
+        # Reevaluate centers
+        mu = reevaluate_centers(clusters)
+        if has_converged(mu, oldmu): 
+            break
+    if False:
+        print
+        print mu
+        print k, len(mu)
+        #print clusters
+        exit()
+    return mu, clusters
+
+
+def Wk(mu, clusters):
+    """Intra-cluster distances"""
+    k = len(mu)
+    return sum(np.linalg.norm(mu[i] - c)**2/(2*len(c)) 
+               for i in range(k) 
+               for c in clusters[i])
+
+
+def bounding_box(X):
+    xmin, xmax = min(X, key=lambda a:a[0])[0], max(X, key=lambda a:a[0])[0]
+    ymin, ymax = min(X, key=lambda a:a[1])[1], max(X, key=lambda a:a[1])[1]
+    return (xmin, xmax), (ymin, ymax)
+
+
+B = 10    
+n_K_0 = 1
+n_K_1 = 10
+
+def gap_statistic(X):
+    """Return
+        ks: k values
+        Wks: log(intra-cluster distance) for each k
+        Wkbs: average referemce log(intra-cluster distance) for each k
+        sk: Normalized std dev log(intra-cluster distance) for each k
+    """
+    (xmin, xmax), (ymin, ymax) = bounding_box(X)
+    # Dispersion for real distribution
+    ks = range(n_K_0, n_K_1)
+    Wks = np.zeros(len(ks))
+    Wkbs = np.zeros(len(ks))
+    sk = np.zeros(len(ks))
+    for indk, k in enumerate(ks):
+        mu, clusters = find_centers(X, k)
+        Wks[indk] = np.log(Wk(mu, clusters))
+
+        # Create B reference datasets
+        BWkbs = np.zeros(B)
+        for i in range(B):
+            Xb = np.vstack([np.random.uniform(xmin, xmax, X.shape[0]),
+                            np.random.uniform(ymin, ymax, X.shape[0])]).T
+            mu, clusters = find_centers(Xb, k)
+            BWkbs[i] = np.log(Wk(mu, clusters))
+        Wkbs[indk] = sum(BWkbs)/B
+        sk[indk] = np.sqrt(sum((BWkbs-Wkbs[indk])**2)/B)
+    sk = sk*np.sqrt(1+1/B)
+    return ks, Wks, Wkbs, sk    
+
+
+#mport random
+ 
+def init_board(N):
+    return np.array([(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(N)])
+
+
+def init_board_gauss(N, k, r):
+    n = N/k
+    X = np.empty((N, 2))
+
+    def add_cluster(X, j0, j1, cx, cy, s):
+        j = j0
+        while j < j1:
+            a, b = np.random.normal(cx, s), np.random.normal(cy, s)
+            # Continue drawing points from the distribution in the range [-1, 1]
+            if abs(a) < 1 and abs(b) < 1:
+                X[j, :] = a, b
+                j += 1
+
+    def d2(x1, x2): 
+        return np.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2) 
+
+    best_dist = 0
+    best_clusters = None
+    corners = [(x, y) for x in (-1.0, 1.0) for y in (-1.0, 1.0)] 
+    for _ in range(1000):
+        clusters = [(np.random.uniform(-1, 1), np.random.uniform(-1, 1)) 
+                    for _ in range(k)]
+        dist = 0
+        for i, x in enumerate(clusters):
+            assert -1 <= x[0] <= 1, x[0]
+            assert -1 <= x[1] <= 1, x[1]
+            dist += min(x[0] + 1.0, 1.0 - x[0])
+            dist += min(x[1] + 1.0, 1.0 - x[1])
+            
+            #print dist,
+            for j in range(k):
+                if j == i: continue
+                dist += d2(x, clusters[j])
+            #print dist    
+        if dist > best_dist:
+            best_dist, best_clusters = dist, clusters
+
+    clusters = []            
+    for i in range(k):
+        cx, cy = best_clusters[i] # np.random.uniform(-1, 1), np.random.uniform(-1, 1)
+        s = r # np.random.uniform(r, r)
+        j0, j1 = int(round(i * n)), int(round((i + 1) * n))
+        add_cluster(X, j0, j1, cx, cy, s)
+        clusters.append([cx, cy, s])
+   
+    return X, clusters    
+ 
+ 
+import matplotlib.pyplot as plt 
+
+random.seed(111) 
+np.random.seed(111) 
+
+
+def pbb(X, c=None):
+    return
+    (xmin, xmax), (ymin, ymax) = bounding_box(X)
+    print '(%5.2f, %5.2f) (%5.2f, %5.2f) %s' % (xmin, xmax, ymin, ymax, X.shape),
+    if c:
+        cx, cy, s = c
+        print '(%5.2f, %5.2f %5.2f)' % (cx, cy, s),
+    print    
+
+def test(actual_k, N, r, do_graph=False):
+
+    X, centroids = init_board_gauss(N, actual_k, r)  
+    mu, clusters2 = find_centers(X, actual_k) 
+    #print actual_k, N, r, do_graph
+    #print len(centroids), len(mu)
+    #for i, (c, m) in enumerate(zip(centroids, mu)):
+    #    print c, m
+
+    pbb(X)    
+
+    if do_graph:
+        k = actual_k
+        color_map = ['c', 'k', 'y', 'm']
+        marker_map = ['v', 'o', 's', 'o']
+        fig, ax = plt.subplots()
+        n0 = 0 
+        for i in range(k):
+            n = N * (i + 1)/k
+            x = X[n0:n, :]
+            n0 = n
+            pbb(x, centroids[i])
+            ax.scatter(x[:, 0], x[:, 1], 
+                c=color_map[i % len(color_map)], 
+                marker=marker_map[i % len(marker_map)])
+
+        ax.set_xlabel(r'x', fontsize=20)
+        ax.set_ylabel(r'y', fontsize=20)
+        ax.set_title('Clusters')
+        plt.xlim((-1.0, 1.0))
+        plt.ylim((-1.0, 1.0))
+
+        ax.grid(True)
+        #fig.tight_layout()    
+        plt.show()
+
+
+    ks, logWks, logWkbs, sk = gap_statistic(X) 
+    if False:
+        print 'ks', ks 
+        print 'logWks', logWks 
+        print 'logWkbs', logWkbs
+        print 'sk',  sk    
+
+    results = zip(ks, logWks, logWkbs, sk)
+    ok = None
+    predicted_k = -1
+
+    for i, (k, wk, wkb, sk) in enumerate(results):
+        gap = wkb - wk
+        if i + 1 < len(results):
+            _, wk1, wkb1, sk1 = results[i + 1]  
+            gap1 = wkb1 - wk1    
+            ok = gap > gap1 - sk1
+            if ok and predicted_k < 0:
+                predicted_k = k
+        #print '%2d %5.2f %5.2f %5.2f : %5.2f %s' % (k, wk, wkb, sk, gap, ok)
+        
+    correct = predicted_k == actual_k
+    print 'N=%3d,k=%d,predicted_k=%d,correct=%s' % (N, actual_k, predicted_k, correct)
+    sys.stdout.flush()
+    #exit()
+    return correct
+
+    
+r = 0.01    
+test(16, 200, r, do_graph=True)    
+    
+M = 10    
+results = []
+for N in (20, 50, 100, 200, 400, 1000):
+    for k in (1, 2, 3, 5, 7, 10, 20)[1:]:
+        if k**2 > N: continue
+        m = sum(test(k, N, r) for _ in range(M))
+        results.append((N, k, m))
+        print 'N=%3d,k=%2d:  %d of %d = %3d%%' % (N, k, m, M, int(100.0 * m/ M))
+        print '-' * 80
+        sys.stdout.flush()
+
+for N, k, m in results:
+    print 'N=%3d,k=%2d:  %d of %d = %3d%%' % (N, k, m, M, int(100.0 * m/ M))
+
